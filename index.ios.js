f33c0d30e8dd44aa923768f8ac25e53f7fbfae35
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-05 18:50:47 +0700

    ong

---
+++ index.ios.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, Button } from 'react-native';
+
+function a() {
+  return () => console.log('123');
+}
+
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 1 };
+  }
+
+  a(num) {
+    console.log(num);
+  } 
+
+  render() {
+    return (
+      <View>
+        <Text style={{ color: this.props.mau }}>Hello {this.props.name}</Text>
+        <Text>{this.state.value}</Text>
+        <Button title="Add" onPress={a()} />
+      </View>
+    );
+  }
+}
+
+const Hi = (props) => (
+  <View>
+    <Text>Hi Component {props.name}</Text>
+  </View>
+);
+
+class Box extends Component {
+  render() {
+    return <Button title="0"/>
+  }
+}
+
+class HelloWorldApp extends Component {
+  render() {
+    return (
+      <View>
+        <Box />
+        <Box />
+        <Box />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('Demo', () => HelloWorldApp);
